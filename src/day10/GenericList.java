4b85131551fbc192c3ecf7b9f2205a5e8797b120
Tezla-Lee <tezla.lee@gmail.com>
2020-08-14 12:01:05 +0900

    Modify data type of array

--- src/day10/GenericList.java
+++ src/day10/GenericList.java
@@ -2,46 +2,50 @@ package day10;
 
 /**
  * List<T> 제네릭 인터페이스를 구현하여 ArrayList<T> 제네릭을 완성하시오.
- *
+ * <p>
  * List 는 순서가 있는 연속된 값으로, List 인터페이스에 선언되어 있는 메소드의 기능은 아래와 같다.
- *
+ * <p>
  * - append(): List 의 마지막에 value 를 삽입한다.
  * - prepend(): List 의 시작점에 value 를 삽입한다. 기존의 데이터를 한 칸씩 뒤로 옮긴다.
  * - insert(): index 에 value 를 삽입한다. 기존 데이터를 한 칸씩 뒤로 옮긴다.
  * - remove(): index 의 value 를 삭제한다. 기존 데이터를 한 칸씩 앞으로 당긴다.
  * - get(): index 의 value 를 반환한다.
  * - length(): List 의 길이를 출력한다.
- *
+ * <p>
  * ArrayList<T>는 Object []를 이용하여 List<T>를 구현한다.
  * - 생성자에서는 capacity 를 입력받으며, 배열의 크기가 부족할 때마다 2배씩 증가시킨다.
  */
 
 interface List<T> {
     void append(T value);
+
     void prepend(T value);
+
     void insert(int index, T value);
+
     void remove(int index);
+
     T get(int index);
+
     int length();
 }
 
-class ArrayList<T> implements List<T>{
+class ArrayList<T> implements List<T> {
     private int capacity;
     private int length = 0;
     private Object[] array;
 
-    public ArrayList(int capacity) {
+    public <T extends Object> ArrayList(int capacity) {
         this.capacity = capacity;
         array = new Object[capacity];
     }
 
     private void expandCapacity(int offset) {
-        Object [] new_array = new Object[capacity * 2];
+        Object[] new_array = new Object[capacity * 2];
         System.arraycopy(array, 0, new_array, offset, capacity);
         array = new_array;
         capacity *= 2;
     }
-
 
 
     @Override
@@ -58,7 +62,7 @@ class ArrayList<T> implements List<T>{
         if (length == array.length) {
             expandCapacity(1);
         } else {
-            System.arraycopy(array, 0 , array, 1, length);
+            System.arraycopy(array, 0, array, 1, length);
         }
         array[0] = value;
         length++;
@@ -96,6 +100,7 @@ class ArrayList<T> implements List<T>{
     public int length() {
         return length;
     }
+
     public void printList(ArrayList<T> list) {
         for (int i = 0; i < list.length(); i++) {
             System.out.print(array[i] + " ");
@@ -107,7 +112,7 @@ class ArrayList<T> implements List<T>{
 
 public class GenericList {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>(10);
+        ArrayList<Object> list = new ArrayList<>(10);
         for (int i = 0; i < 20; i++) {
             list.append(i);
         }
@@ -121,14 +126,17 @@ public class GenericList {
         list.prepend(3);
         list.printList(list);
 
-        list.insert(5, 100);
+        list.insert(5, "abc");
+        list.prepend("abcd");
+        list.prepend('a');
         list.printList(list);
+//        list.prepend("abc");
         System.out.println();
 
 
-        ArrayList<Character> listChar = new ArrayList<>(10);
+        ArrayList<Object> listChar = new ArrayList<>(10);
         for (int i = 0; i < 20; i++) {
-            char ch = (char) (i + 65) ;
+            char ch = (char) (i + 65);
             listChar.append(ch);
         }
         listChar.printList(listChar);
