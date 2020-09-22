36ef9c7b2a1fcb734095418f4e9a4ad0a6968342
Tezla-Lee <tezla.lee@gmail.com>
2020-09-22 21:25:41 +0900

    Homework #12

--- src/day12/LambdaExpressions.java
+++ src/day12/LambdaExpressions.java
@@ -18,9 +18,7 @@ class Algorithm<T, R> {
 
 public class LambdaExpressions {
     public static void main(String[] args) {
-        /**
-         * 문제 1. n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
-          */
+        // 문제 1. n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
         // case 1 : 입력 5, 출력 15
         System.out.println(new Algorithm<>().test(5, 15, x -> {
             int sum = 0;
@@ -39,15 +37,14 @@ public class LambdaExpressions {
             return sum;
         }));
 
-        /** 문제 2. 가운데 글자 가져오기
-         *  - 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
-         */
+        // 문제 2. 가운데 글자 가져오기
+        //  - 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환.
         // case 1 : 입력 "abcde", 출력 "c"
         System.out.println(new Algorithm<String, String>().test("abcde", "c", s -> {
             String answer = "";
             int len = s.length();
-            if (len %2 == 0) {
-                char [] arr = {s.charAt(len / 2 - 1), s.charAt(len / 2)};
+            if (len % 2 == 0) {
+                char[] arr = {s.charAt(len / 2 - 1), s.charAt(len / 2)};
                 answer = String.valueOf(arr);
             } else {
                 answer = Character.toString(s.charAt(len / 2));
@@ -59,8 +56,8 @@ public class LambdaExpressions {
         System.out.println(new Algorithm<String, String>().test("qwer", "we", s -> {
             String answer = "";
             int len = s.length();
-            if (len %2 == 0) {
-                char [] arr = {s.charAt(len / 2 - 1), s.charAt(len / 2)};
+            if (len % 2 == 0) {
+                char[] arr = {s.charAt(len / 2 - 1), s.charAt(len / 2)};
                 answer = String.valueOf(arr);
             } else {
                 answer = Character.toString(s.charAt(len / 2));
