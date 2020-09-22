19717fd8f3062fa0d9ccdb561cc0361eba69fe7b
Tezla-Lee <tezla.lee@gmail.com>
2020-09-22 21:26:59 +0900

    Homework #12

--- src/day12/LambdaExpressions.java
+++ src/day12/LambdaExpressions.java
@@ -26,7 +26,7 @@ public class LambdaExpressions {
                 sum += i;
             }
             return sum;
-        }));
+        })); // true
 
         // case 2 : 입력 3, 출력 6
         System.out.println(new Algorithm<>().test(3, 6, x -> {
@@ -35,7 +35,7 @@ public class LambdaExpressions {
                 sum += i;
             }
             return sum;
-        }));
+        })); // true
 
         // 문제 2. 가운데 글자 가져오기
         //  - 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환.
@@ -50,7 +50,7 @@ public class LambdaExpressions {
                 answer = Character.toString(s.charAt(len / 2));
             }
             return answer;
-        }));
+        })); // true
 
         // case 2 : 입력 "qwer", 출력 "we"
         System.out.println(new Algorithm<String, String>().test("qwer", "we", s -> {
@@ -63,6 +63,6 @@ public class LambdaExpressions {
                 answer = Character.toString(s.charAt(len / 2));
             }
             return answer;
-        }));
+        })); // true
     }
 }
